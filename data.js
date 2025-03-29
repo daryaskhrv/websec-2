bdbb0a7c59b6264676d096bbe0172f1cae8f4102
Daryaskhrv <daryasheqer@yandex.ru>
2025-03-29 21:13:52 +0400

    html и стилизация

--- data.js
+++ data.js
@@ -1,8 +1,6 @@
-// Функция для форматирования даты
 function formatCurrentDate() {
     const now = new Date();
     
-    // Настройки для локали (русский язык)
     const options = {
         weekday: 'short',
         day: '2-digit',
@@ -15,7 +13,7 @@ function formatCurrentDate() {
 
 function updateDateTime() {
     document.querySelector('.data').textContent = formatCurrentDate();
-    setTimeout(updateDateTime, 1000 * 60 * 60 * 24); // Обновлять раз в сутки
+    setTimeout(updateDateTime, 1000 * 60 * 60 * 24); 
 }
 
-updateDateTime(); // Первоначальный вызов
+updateDateTime(); 
